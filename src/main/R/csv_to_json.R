9352edfa9b649ae11191d61977c3f35113a39557
gezever <gezever@skynet.be>
2022-10-05 18:33:32 +0200

    dct:isReferencedBy naar LUC in lijst van observatieprocedures rdfs:comment naar vlarem in tekstvelden

--- src/main/R/csv_to_json.R
+++ src/main/R/csv_to_json.R
@@ -52,3 +52,4 @@ write(df_in_json, "/tmp/chemische_stof.jsonld")
 # hiervoor dienen jena cli-tools geinstalleerd, zie README.md
 system("riot --formatted=TURTLE /tmp/chemische_stof.jsonld > ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/chemische_stof.ttl")
 system("riot --formatted=JSONLD ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/chemische_stof.ttl > ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/chemische_stof.jsonld")
+system("shacl v --shapes ../resources/be/vlaanderen/omgeving/data/id/ontology/chemische-stof-ap-constraints/chemische-stof-ap-constraints.ttl --data ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/chemische_stof.ttl")
