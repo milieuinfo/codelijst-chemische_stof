349e4bd5ea10aa8004cd9d0c258f35beccabf06f
gezever <gezever@skynet.be>
2023-09-27 17:19:37 +0200

    fix https://github.com/milieuinfo/codelijst-observatieprocedure/issues/1

--- src/main/bash/01_csv_to_rfd.sh
+++ src/main/bash/01_csv_to_rfd.sh
@@ -3,4 +3,4 @@
 # Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/chemische_stof.csv
 # to jsonld, /tmp/chemische_stof.jsonld
 Rscript ../R/csv_to_json.R
-
+shacl v --shapes ../resources/be/vlaanderen/omgeving/data/id/ontology/chemische-stof-ap-constraints/chemische-stof-beperkt-ap-constraints.ttl --data ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/chemische_stof.ttl
