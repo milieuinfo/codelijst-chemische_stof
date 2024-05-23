88baf1e695312a45dd693f672933b2348b61bf25
gezever <gezever@skynet.be>
2024-05-23 16:50:52 +0200

    54

--- src/documentation/model.sh
+++ src/documentation/model.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-  sparql --results=TTL --data=../main/resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.ttl  --query model.rq  > model.ttl
+  riot --formatted=turtle ../main/resources/be/vlaanderen/omgeving/data/id/conceptscheme/chemische_stof/*ttl > /tmp/chemische_stof.ttl
+  sparql --results=TTL --data=/tmp/chemische_stof.ttl  --query model.rq  > model.ttl
   rdf2dot  model.ttl | dot -Tpng > model.png
   rdf2dot  model.ttl  > model.dot
